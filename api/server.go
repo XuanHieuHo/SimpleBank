0f46d3728817cb993975c8a511a8c915f14ef5cf
XuanHieu <hoxuanhieu.pch.2b1.92@gmail.com>
2023-03-20 09:38:44 +0700

    [20/03/2023][updated]

--- api/server.go
+++ api/server.go
@@ -1,48 +1,76 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/techschool/simplebank/db/sqlc"
+	"github.com/techschool/simplebank/token"
+	"github.com/techschool/simplebank/util"
 )
 
-//Server serves HTTP requests for our banking service
+// Server serves HTTP requests for our banking service
 type Server struct {
-	store db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
 	// add routes to router
 	// accounts
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id",server.getAccount)
-	router.GET("/accounts",server.listAccount)
-	router.PUT("/accounts/:id",server.updateAccount)
-	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccount)
+	authRoutes.PUT("/accounts/:id", server.updateAccount)
+	authRoutes.DELETE("/accounts/:id", server.deleteAccount)
 
 	//transfers
-	router.POST("/transfers", server.createTransfer)
-	router.GET("/transfers/:id", server.getTransfer)
-	router.GET("/transfers/list/from/:from_account_id/", server.listTransferFrom)
-	router.GET("/transfers/list/to/:to_account_id/", server.listTransferTo)
+	authRoutes.POST("/transfers", server.createTransfer)
+	authRoutes.GET("/transfers/:id", server.getTransfer)
+	authRoutes.GET("/transfers/list/from/:from_account_id/", server.listTransferFrom)
+	authRoutes.GET("/transfers/list/to/:to_account_id/", server.listTransferTo)
+
+	// users
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	
+
 	server.router = router
-	return server
 }
 
 // Start runs thes HTTP server on a specific address
 func (server *Server) Start(address string) error {
 	return server.router.Run(address)
-} 
+}
 
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
